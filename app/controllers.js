0e971be20a4dd6db80884c0d0803c64e4a94c6b4
Chris Petersen <geek@ex-nerd.com>
2012-07-19 00:21:21 -0700

    local storage for data persistence

--- app/controllers.js
+++ app/controllers.js
@@ -4,24 +4,49 @@ function BoozeCtrl($scope, $routeParams) {
     //console.debug($routeParams);
 }
 
-function TaxCtrl($scope, $routeParams) {
+function TaxCtrl($scope, $filter, $routeParams) {
     localStorage.setItem("last_page", "/tax");
+
     //console.debug($scope);
+    //console.debug($filter);
     //console.debug($routeParams);
-    var $filter = angular.injector(['ng', 'booze']).get('$filter');
     var money = $filter('money');
 
     $scope.bottles = [
-            { name: "330ml", size: .33 },
-            { name: "750ml", size: .75 },
-            { name: "1L",    size: 1 }
+            { name: "330ml", size: '.33' },
+            { name: "750ml", size: '.75' },
+            { name: "1L",    size: '1' },
+            { name: "1.5L",  size: '1.5' },
+            { name: "1.75L", size: '1.75' },
+            { name: "2L",    size: '2' },
         ];
-    $scope.size = .75;
-    $scope.price = 0.00;
 
+    // Scope variables
+
+    var last_size = localStorage.getItem("last_size");
+    if (isNaN(parseFloat(last_size)) || parseFloat(last_size) == 0 ) {
+        last_size = '.75';
+    }
+    $scope.size = last_size;
+    //console.debug('last_size: ' + $scope.size);
+
+    var last_price = parseFloat(localStorage.getItem("last_price"));
+    if (isNaN(last_price)) {
+        last_price = 0;
+    }
+    $scope.price = last_price; // see bug below for why we can't make this pretty
+    //console.debug('last price: ' + $scope.price);
+
+    // Scope methods
+
+    $scope.save_state = function() {
+        localStorage.setItem("last_size", $scope.size);
+        localStorage.setItem("last_price", $scope.price);
+        console.log('save state');
+    };
     $scope.select_price = function() {
         $('#tax-price').get(0).setSelectionRange(0, 9999);
-    }
+    };
     $scope.format_price = function() {
         // Disabled until fixed: https://github.com/angular/angular.js/issues/1126
         //$scope.price = money($scope.price);
